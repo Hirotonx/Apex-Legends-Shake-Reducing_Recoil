1bd24274e25eb0cc15d26f33f399c7b46fb263f5
H <96516810+Hirotonx@users.noreply.github.com>
2022-06-10 13:15:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import win32api
 from time import sleep,time
+Shake=[[-2,2,-2,2,-2,-1,2,1],[3,-3,3,-3,2,2,-2,-2]]
 def left_click_state():#If the left mouse button has been pressed return True 返回左键是否按下
     left_click = win32api.GetKeyState(0x01)
     return left_click < 0
@@ -9,55 +10,41 @@ def right_click_state():
 def end_click_state():
     end_click = win32api.GetKeyState(0x23)
     return end_click < 0
+def dou():
+    k=0
+    if left_click_state() and not right_click_state(): #此代码可以压前几枪
+     while(k<4):
+        sleep(0.05)
+        win32api.mouse_event(0x0001, 0, -15)
+        k+=1
 def Dou2():#shake 抖动
     time1=0.015
     time2=0.025
-    win32api.mouse_event(0x0001, -2, 3)
-    sleep(time2)
-    if left_click_state() and not right_click_state():
-        win32api.mouse_event(0x0001, 2, -3)
-        sleep(time2)
-    if left_click_state() and not right_click_state():
-        win32api.mouse_event(0x0001, -2, 3)
-        sleep(time2)
-    if left_click_state() and not right_click_state():
-        win32api.mouse_event(0x0001, 2, -3)
-        sleep(time2)
+    global Shake
+    for i in range(0,4):
+        if left_click_state() and not right_click_state():
+            win32api.mouse_event(0x0001, Shake[0][i], Shake[1][i])
+            sleep(time2)
 def Dou1():
     time1=0.015
     time2=0.025
-    win32api.mouse_event(0x0001, -2, 2)
-    sleep(time2)
-    if left_click_state() and not right_click_state():
-        win32api.mouse_event(0x0001, -1, 2)
-        sleep(time2)
-    if left_click_state() and not right_click_state():
-        win32api.mouse_event(0x0001, 2, -2)
-        sleep(time2)
-    if left_click_state() and not right_click_state():
-        win32api.mouse_event(0x0001, 1, -2)
-        sleep(time2)
+    global Shake
+    for i in range(4,8):
+        if left_click_state() and not right_click_state():
+            win32api.mouse_event(0x0001, Shake[0][i], Shake[1][i])
+            sleep(time2)
 def main():
-    k=0
     while True:
         if end_click_state():
             return
         sleep(0.001)
         if right_click_state():
                  temp = 0
-                 # if left_click_state() and not right_click_state(): #此代码可以压前几枪
-                 #    while(k<4):
-                 #        sleep(0.05)
-                 #        win32api.mouse_event(0x0001, 0, -15)
-                 #        k+=1
                  while True:
                     sleep(0.001)
                     start = time()
-                    if left_click_state() and not right_click_state():
-                        Dou2()
-                    sleep(0.001)
-                    if left_click_state() and not right_click_state():
-                        Dou1()
+                    Dou2()
+                    Dou1()
                     end = time()
                     temp+=end-start
                     sleep(0.001)
