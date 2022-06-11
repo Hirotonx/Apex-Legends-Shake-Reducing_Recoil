34f03003e23dee86a10757a20ae9e6dd605e8408
H <96516810+Hirotonx@users.noreply.github.com>
2022-06-11 17:39:39 +0800

    Create main.py
    添加了识别模块

--- main.py
+++ main.py
@@ -1,42 +1,50 @@
-"'https://github.com/Hirotonx/Apex-Legends-Shake-Reducing_Recoil'"
 import win32api
 from time import sleep,time
-Shake0=[[-2,2,-2,2,-2,-1,2,1],[3,-3,3,-3,2,2,-2,-2]]
-Shake1=[[-1,1,-1,1,-1,-1,1,1],[1,-1,1,-1,1,-1,1,-1]]
-#If the left mouse button has been pressed return True 返回左键是否按下
-def left_click_state():
+from recognition import regulation_color
+Shake=[[-2,2,-2,2,-2,-1,2,1],[3,-3,3,-3,2,2,-2,-2]]
+open_regulation=True#激活识别瞄准镜
+
+def left_click_state():#If the left mouse button has been pressed return True 返回左键是否按下
     left_click = win32api.GetKeyState(0x01)
     return left_click < 0
+
 def right_click_state():
     right_click = win32api.GetKeyState(0x02)
     return right_click < 0
+
 def end_click_state():
     end_click = win32api.GetKeyState(0x23)
     return end_click < 0
+
 def dou():
-    k=0
-    if left_click_state() and not right_click_state(): #此代码可以压前几枪
-     while(k<4):
-        sleep(0.045)
-        win32api.mouse_event(0x0001, 0, 15)
-        k+=1
+    while True:
+        sleep(0.001)
+        k=0
+        if left_click_state() and not right_click_state()and open_regulation and regulation_color(): #此代码可以压前几枪
+         while(k<4):
+            sleep(0.06)
+            win32api.mouse_event(0x0001, 0, 7)
+            k+=1
+        if(k==4):
+            break
 def Dou2():#shake 抖动
     time1=0.015
     time2=0.025
     global Shake
     for i in range(0,4):
-        if left_click_state() and not right_click_state():
-            win32api.mouse_event(0x0001, Shake1[0][i], Shake1[1][i])
-            sleep(time1)
+        if left_click_state() and not right_click_state()and open_regulation and regulation_color():
+            win32api.mouse_event(0x0001, Shake[0][i], Shake[1][i])
+            sleep(time2)
 def Dou1():
     time1=0.015
     time2=0.025
     global Shake
     for i in range(4,8):
-        if left_click_state() and not right_click_state():
-            win32api.mouse_event(0x0001, Shake1[0][i], Shake1[1][i])
-            sleep(time1)
+        if left_click_state() and not right_click_state()and open_regulation and regulation_color():
+            win32api.mouse_event(0x0001, Shake[0][i], Shake[1][i])
+            sleep(time2)
 def main():
+    global open_regulation
     while True:
         if end_click_state():
             return
@@ -48,8 +56,6 @@ def main():
                     sleep(0.001)
                     start = time()
                     Dou2()
-                    if right_click_state():
-                        temp=0
                     Dou1()
                     end = time()
                     temp+=end-start
